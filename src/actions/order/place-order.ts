4c746b190ad37b54ceda532244849a944fc039fb
DaviSnake <dmedinac@gmail.com>
2024-06-27 06:31:10 -0400

    Colocar orden - Colocar productos y cantidades

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,8 @@
 
 import { auth } from "@/auth.config";
 import type{ Address, Size } from "@/interfaces";
+import prisma from "@/lib/prisma";
+import { count } from "console";
 
 interface ProductToOrder {
 
@@ -16,6 +18,7 @@ export const placeOrder = async( productIds: ProductToOrder[], addrees: Address)
     const session = await auth();
     const userId = session?.user.id;
 
+    //Verificar sesion de usuario.
     if ( !userId ) {
         return {
             ok: false,
@@ -23,7 +26,23 @@ export const placeOrder = async( productIds: ProductToOrder[], addrees: Address)
         }
     }
 
-    console.log({productIds, addrees, userId});
+    //Obtenr las informacion de los productos 
+    //Nota: Podemos llevar 2 o mas productos con el mismo id
+    const products = await prisma.product.findMany({
+        where: {
+            id: {
+                in: productIds.map( p => p.productId)
+            }
+        }
+    });
+
+   //Carcular los montos, Encabezado
+   const itemsInOrder = productIds.reduce( (count, p ) => count + p.quantity, 0 );
+
+   console.log(itemsInOrder);
+
+
+   
 
 
 }
