a81bd6273c627e317b625f944df3880cdb5f7bc3
DaviSnake <dmedinac@gmail.com>
2024-07-07 02:58:16 -0400

    CustomImage - Componente

--- src/app/(shop)/admin/products/page.tsx
+++ src/app/(shop)/admin/products/page.tsx
@@ -1,6 +1,6 @@
 // https://tailwindcomponents.com/component/hoverable-table
 import { getPaginatedProductsWithImages } from "@/actions";
-import { Pagination, Title } from "@/components";
+import { Pagination, ProductImage, Title } from "@/components";
 import Image from "next/image";
 
 import Link from "next/link";
@@ -78,8 +78,8 @@ export default async function ProductsPage({ searchParams }: Props) {
               className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
                 <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
                   <Link href={ `/product/${ product.slug }` }>
-                    <Image 
-                    src={ `/products/${ product.ProductImage[0].url }`}
+                    <ProductImage 
+                    src={ product.ProductImage[0]?.url }
                     width={ 80 }
                     height={ 80 }
                     alt={ product.title }
